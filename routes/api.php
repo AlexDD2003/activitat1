bca7f6f9fbffe1023e7bf847df9ed29127fbb207
AlexDD2003 <a.diaz2daw@gmail.com>
2023-04-17 17:29:12 +0200

    Acabado

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::resource('/coche', CocheController::class)->middleware('auth:sanctum');
-
+Route::put('/coche/{id}', [CocheController::class, 'update']);
+Route::delete('/coche/{id}', [CocheController::class, 'destroy']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
