d56bbd680747702dac0d4337ae44d6964f5ca1c2
AlexDiazDuran <a.diaz2daw@gmail.com>
2023-04-17 12:01:50 +0200

    CRUD

---
+++ activitat1/app/Http/Controllers/CocheController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CocheController extends Controller
+{
+    public function index()
+    {
+        $coches = Coche::all();
+        return response()->json($coches, 200);
+    }
+
+    public function show($id)
+    {
+        $coche = Coche::find($id);
+        if($coche){
+            return response()->json($coche, 200);
+        } else {
+            return response()->json(['error' => 'Coche no encontrado'], 404);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $coche = new Coche;
+        $coche->marca = $request->marca;
+        $coche->modelo = $request->modelo;
+        $coche->color = $request->color;
+        $coche->precio = $request->precio;
+        $coche->save();
+        return response()->json($coche, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $coche = Coche::find($id);
+        if($coche){
+            $coche->marca = $request->marca;
+            $coche->modelo = $request->modelo;
+            $coche->color = $request->color;
+            $coche->precio = $request->precio;
+            $coche->save();
+            return response()->json($coche, 200);
+        } else {
+            return response()->json(['error' => 'Coche no encontrado'], 404);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $coche = Coche::find($id);
+        if($coche){
+            $coche->delete();
+            return response()->json(null, 204);
+        } else {
+            return response()->json(['error' => 'Coche no encontrado'], 404);
+        }
+    }
+}
--- activitat1/routes/web.php
+++ activitat1/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/coches', 'App\Http\Controllers\CocheController@index');
+Route::post('/coches', 'App\Http\Controllers\CocheController@store');
+Route::get('/coches/{id}', 'App\Http\Controllers\CocheController@show');
+Route::put('/coches/{id}', 'App\Http\Controllers\CocheController@update');
+Route::delete('/coches/{id}', 'App\Http\Controllers\CocheController@destroy');
+
+
