8e8d458fe90cacb4958e8199e7a8e5c7b7dc0e9e
AlexDD2003 <a.diaz2daw@gmail.com>
2023-04-17 16:56:20 +0200

    Finish

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Laravel\Sanctum\HasApiTokens;
+
+class AuthController extends Controller
+{
+    use HasApiTokens;
+
+    /**
+     * Register a new user.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $user->save();
+
+        $token = $user->createToken('MyAppToken')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Successfully registered.',
+            'token' => $token,
+            'user' => $user
+        ], 201);
+    }
+
+    /**
+     * Log the user in and return a token.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $credentials = $request->only(['email', 'password']);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid login credentials.'
+            ], 401);
+        }
+
+        $user = $request->user();
+        $token = $user->createToken('MyAppToken')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Successfully logged in.',
+            'token' => $token
+        ], 200);
+    }
+
+    /**
+     * Log the user out (invalidate the token).
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Get the authenticated user.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function user(Request $request)
+    {
+        return response()->json($request->user(), 200);
+    }
+}
--- app/Http/Controllers/CocheController.php
+++ app/Http/Controllers/CocheController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Coche;
 use Illuminate\Http\Request;
 
 class CocheController extends Controller
