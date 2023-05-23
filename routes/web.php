251c72fb86d5aba5ec98a9d36f3bf2a74805ee8a
AlexDD2003 <115020510+AlexDD2003@users.noreply.github.com>
2023-05-23 10:40:09 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -39,10 +39,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/coches', 'CocheController@listarCoches');
-Route::get('/coches', [CocheViewController::class, 'index'])->name('coches.index');
+Route::middleware(['auth'])->get('/coches', [CocheViewController::class, 'index'])->name('coches.index');
 Route::get('/coches/{id}', [CocheViewController::class, 'show'])->name('coches.show');
-Route::get('/carrito', [CartController::class, 'show'])->name('cart.show');
+Route::middleware(['auth'])->get('/carrito', [CartController::class, 'show'])->name('cart.show');
 Route::get('/cart/add/{coche}', [CartController::class, 'add'])->name('cart.add');
 Route::delete('/cart/remove/{coche}', [CartController::class, 'remove'])->name('cart.remove');
 
